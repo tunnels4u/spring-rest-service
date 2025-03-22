68d88a6b0616953ed2a95db33e920cebc68f7a3a
anjali <anjali@localhost>
2025-03-22 14:48:33 +0000

    Modified for scalability through connection pooling

--- src/main/java/com/softwaretunnel/spring_rest_service/config/ApplicationConfig.java
+++ src/main/java/com/softwaretunnel/spring_rest_service/config/ApplicationConfig.java
@@ -15,6 +15,9 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
@@ -33,33 +36,70 @@ class ApplicationConfig {
 
 	@Value("${spring.datasource.password}")
 	private String password;
+	
+	@Value("${spring.datasource.hikari.maximum-pool-size}")
+	private Integer maxPoolSize;
+
+	@Value("${spring.datasource.hikari.minimum-idle}")
+	private Integer minIdle;
+
+	@Value("${spring.datasource.hikari.idle-timeout}")
+	private Integer idleTimeout;
+
+	@Value("${spring.datasource.hikari.pool-name}")
+	private String poolName;
+	
+	@Value("${spring.datasource.hikari.max-lifetime}")
+	private Integer maxLifeTime;
+
+	@Value("${spring.datasource.hikari.connection-timeout}")
+	private Integer connectionTimeout;
+
+//	@Bean
+//	public DataSource dataSource() {
+//
+//		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+//		EmbeddedDatabaseConfigurer embeddedDatabaseConfigurer = new EmbeddedDatabaseConfigurer() {
+//
+//			@Override
+//			public void shutdown(DataSource dataSource, String databaseName) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//
+//			@Override
+//			public void configureConnectionProperties(ConnectionProperties properties, String databaseName) {
+//				properties.setUrl(datasourceUrl);
+//				properties.setUsername(userName);
+//				properties.setPassword(password);
+//				try {
+//					properties.setDriverClass((Class<? extends org.h2.Driver>) Class.forName(driverClass));
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//		builder.setDatabaseConfigurer(embeddedDatabaseConfigurer);
+//		return builder.build();
+//	}
 
 	@Bean
 	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setJdbcUrl(datasourceUrl); // File-based H2
+		hikariConfig.setDriverClassName(driverClass);
+		hikariConfig.setUsername(userName);
+		hikariConfig.setPassword(password);
 
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabaseConfigurer embeddedDatabaseConfigurer = new EmbeddedDatabaseConfigurer() {
-
-			@Override
-			public void shutdown(DataSource dataSource, String databaseName) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void configureConnectionProperties(ConnectionProperties properties, String databaseName) {
-				properties.setUrl(datasourceUrl);
-				properties.setUsername(userName);
-				properties.setPassword(password);
-				try {
-					properties.setDriverClass((Class<? extends org.h2.Driver>) Class.forName(driverClass));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		builder.setDatabaseConfigurer(embeddedDatabaseConfigurer);
-		return builder.build();
+		// scalability of db
+		hikariConfig.setMaximumPoolSize(maxPoolSize);
+        hikariConfig.setMinimumIdle(minIdle);
+        hikariConfig.setIdleTimeout(idleTimeout);
+        hikariConfig.setMaxLifetime(maxLifeTime);
+        hikariConfig.setConnectionTimeout(connectionTimeout);
+        hikariConfig.setPoolName(poolName);
+		
+		return new HikariDataSource(hikariConfig);
 	}
 
 	@Bean
